f4dafd7800c1a86616fc1d29e35f5d22f25f0c60
rishabhvermagrid <rverma@griddynamics.com>
2026-03-20 19:09:50 +0530

    how to create a singleton class

--- Main.java
+++ Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class Person {
+    private String name;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    // single instance
+    private static final Person instance = new Person();
+
+
+    // private constructor
+    private Person() {}
+
+    // global access point
+    public static Person getInstance() {
+        return instance;
+    }
+
+    // setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // getter
+    public String getName() {
+        return name;
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+
+        // get instance
+        Person p1 = Person.getInstance();
+        p1.setName("Rishabh");
+        p1.setName("Rishab");
+
+        // get instance again
+        Person p2 = Person.getInstance();
+        p2.setName("dinesh");
+
+        // check values
+        System.out.println(p1.getName()); // Rishabh
+
+        // check both references
+        System.out.println(p1 == p2); // true
+    }
+}
+/*
+Both p1 and p2 point to the same object (only one instance exists).
+ */
